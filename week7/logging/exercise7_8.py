c3d384794fd38ceee7e9bda8b32f9e886b56720f
Meir Leviev <meirleviev@gmail.com>
2026-05-30 21:06:25 +0300

    Add q9

--- week7/logging/exercise7_8.py
+++ week7/logging/exercise7_8.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s')
@@ -30,3 +32,18 @@ def read_config(filepath):
     except FileNotFoundError:
         logger.exception('not good')
         return None
+
+#9
+def write_structured_log(message, module, level, **extra):
+    log_dict = {
+        "timestamp": datetime.utcnow().isoformat(),
+        "level": level,
+        "module": module,
+        "message": message
+    }
+
+    log_dict.update(extra)
+    json_output = json.dumps(log_dict)
+    print(json_output)
+
+write_structured_log("INFO", "auth", "User logged in", user_id=42)
